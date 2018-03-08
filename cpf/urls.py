48c981b8faf97266ea9561af184d79cc096066d0
Adalto Picotti Junior <adalto@localhost.localdomain>
2018-03-07 23:33:33 -0300

    add templates base

---
+++ cpf/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^res/', views.res, name='res'),
+]
--- cpf/views.py
+++ cpf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, "cpf/home.html")
+
+def res(request):
+    return render(request, "structure/base.html")
