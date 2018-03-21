6aad7f9d3b0523dab25c71b39cb01438e5dc693b
Adalto Picotti Junior <adalto@localhost.localdomain>
2018-03-20 22:30:34 -0300

    fix login page, login and profile menu dropdown

--- website/forms.py
+++ website/forms.py
@@ -14,9 +14,9 @@ class SignUpForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=30, required=False, help_text='Optional.',
+    username = forms.CharField(max_length=30, required=False, help_text='Informe seu usuário.',
         widget=forms.TextInput(
-            attrs={'class':'form-control', 'type':'login', 'placeholder':'login'}))
-    password = forms.CharField(max_length=30, required=False, help_text='Optional.',
+            attrs={'class':'form-control', 'type':'login', 'placeholder':'Digite seu usuário...'}))
+    password = forms.CharField(max_length=30, required=False, help_text='Informe sua senha.',
         widget=forms.TextInput(
-            attrs={'class':'form-control', 'type':'password', 'placeholder':'Password'}))
+            attrs={'class':'form-control', 'type':'password', 'placeholder':'Digite sua senha...'}))
